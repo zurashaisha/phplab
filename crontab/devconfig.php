ce92c3f83cf8eca1c29eed420b06a6887fb15a79
zurashaisha <zura.shaishmelashvili@gmail.com>
2023-03-02 01:16:16 +0400

    phplab upload

--- crontab/devconfig.php
+++ crontab/devconfig.php
@@ -6,14 +6,14 @@ $date=date('mdY', time());
 
 $device=$conn->query("SELECT dev_name, devserial, ip from devices where vendor='Juniper'");
 foreach ($device as $dev_det) {
-    echo $dev_det["dev_name"]." ".$dev_det["ip"]."<br>";
+    #echo $dev_det["dev_name"]." ".$dev_det["ip"]."<br>";
     $dirname="/var/vsftp/mpls/ftp/Juniper/configs/".$dev_det["dev_name"];
     if (!file_exists($dirname)) {
         mkdir($dirname, 0777, true);
         chown($dirname, 'mpls');
     }
     $command="file copy /config/juniper.conf.gz ftp://mpls:Silknet#1@172.16.4.102/Juniper/configs/".$dev_det["dev_name"]."/".$dev_det["dev_name"]."-".$date."-config.gz routing-instance MGMT";
-    echo $command;
+    #echo $command;
     $upload = ssh_host_result_nojson(long2ip($dev_det["ip"]), $command);
     chmod($dirname."/".$dev_det["dev_name"]."-".$date."-config.gz", 0775);
 }
